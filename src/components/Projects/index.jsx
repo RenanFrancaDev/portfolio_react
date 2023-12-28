74c222e78d51200495ac51d235c59317f359ae1c
Renan Franca <renanfranca.dev@gmail.com>
2023-12-28 15:09:51 -0300

    feat: project component done

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+import { ProjectsService } from "../../api/ProjectsService";
 import "./projects.css";
+import Card from "../Card";
 
 const Projects = () => {
+  const [landing, setLanding] = useState([]);
+
+  const getProjects = async () => {
+    let { data } = await ProjectsService.getProjects();
+
+    data.map((e) => {
+      if (e.type === "landing") {
+        return setLanding([e]);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
   return (
     <section>
-      <h3>Projects</h3>
+      <h1>Projects</h1>
+      {landing.map((item, index) => {
+        console.log(item, "item");
+        return <Card key={index} item={item} />;
+      })}
     </section>
   );
 };
