cf371fe17d724a3cfd7366f6aff6e0b33e5ba09a
Renan Franca <renanfranca.dev@gmail.com>
2024-01-15 14:30:27 -0300

    chore: presentation and about me revised

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -13,16 +13,16 @@ const About = () => {
         </div>
         <div className="description">
           <p>
-            I started my professional career in the industry, as an intern until
-            I became a buyer, always working in Supply Chain. The first time
-            that I have contact with development was in 2022, making Landing
-            pages with HTML, CSS and JavaScript, when I decided to change my
-            career and work with IT, where I am working as a freelancer at the
-            moment. I started a degree in Systems Analysis and Development and
-            delved deeper into programming studies. Today I am a full stack
-            developer, where I took courses and gained knowledge in React.js,
-            TypeScript Node.js, Rest API, MySQL, NoSQL and I intend to learn
-            more and more.
+            I began my professional career in the industry as an intern and
+            eventually became a buyer, consistently working in Supply Chain. My
+            first exposure to development was in 2022 when I started creating
+            Landing pages using HTML, CSS, and JavaScript. This experience led
+            me to make a career change and pursue IT, where I am currently
+            working as a freelancer. I enrolled in a Systems Analysis and
+            Development program and have since immersed myself in programming
+            studies. Today, I am a full stack developer with expertise in
+            React.js, TypeScript, Node.js, Rest API, MySQL, and NoSQL. I am
+            eager to continue learning and expanding my knowledge in this field.
           </p>
         </div>
       </div>
--- src/components/Presentation/index.jsx
+++ src/components/Presentation/index.jsx
@@ -5,26 +5,29 @@ const Presentation = () => {
   return (
     <section id="presentation">
       <div className="home_container">
-        <h1>Renan França</h1>
+        <h2>Renan França</h2>
         <h3>Full Stack Developer</h3>
         <p>
-          Hello, I'm a production engineering with experience in the supply
-          chain field. Recently, I transitioned to the technology industry and I
-          am currently studying Analysis and Systems Development at the
-          University of Taubaté and a full stack technology program at DNC
-          School. I am currently providing services for a fintech company and
-          also working as a freelance developer. With my engineering background
-          and knowledge in technology, I am constantly seeking to improve my
-          skills and create innovative solutions. I am passionate about
-          developing efficient and user-friendly systems that meet the needs of
-          companies. Through my experience in the supply chain field and my
-          current focus on technology, I have developed a strong understanding
-          of the various aspects of the development process. I am always eager
-          to take on new challenges and collaborate with others to create
-          impactful projects. My goal is to contribute to the success of
-          organizations by leveraging my technical skills and problem-solving
-          abilities. Please consider my portfolio and feel free to reach out to
-          me with any questions. Thank you! work.
+          I'm a production engineer experienced in the supply chain industry and
+          I transitioned to the tech sector and am currently studying Analysis
+          and Systems Development at the University of Taubaté, as well as a
+          full stack technology program at DNC School. Currently, I provide
+          services for a fintech company and also work as a freelance developer.
+          <br />
+          <br />
+          With my engineering background and tech knowledge, I constantly strive
+          to enhance my skills and create innovative solutions. I am passionate
+          about developing efficient and user-friendly systems that cater to
+          companies needs. My experience in the supply chain field, combined
+          with my current focus on technology, has given me a well -rounded
+          understanding of the development process. I am always ready to tackle
+          new challenges and collaborate with others to create impactful
+          projects. My ultimate goal is to contribute to organizational success
+          by leveraging my technical expertise and problem-solving abilities.
+          <br />
+          <br />
+          Please review my portfolio and feel free to contact me if you have any
+          questions.
         </p>
       </div>
       <div className="presentation_img">
